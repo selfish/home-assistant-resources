cbc25383f939d8f5c9223766d702d8e7c2f8b6df
selfish <7327741+selfish@users.noreply.github.com>
2023-11-27 17:11:54 +0200

    Update set_state to support allow_create correctly

--- config/python_scripts/set_state.py
+++ config/python_scripts/set_state.py
@@ -1,34 +1,25 @@
-"""
-python_scripts/set_state.py
-
-Set the state or other attributes for a specified entity.
-Enhancements include error handling, data validation, and user feedback.
-Source:
-https://github.com/selfish/home-assistant-resources/blob/master/config/python_scripts/set_state.py
-
-Adopted based on
-https://github.com/rodpayne/home-assistant/blob/main/.homeassistant/python_scripts/set_state.py
-"""
+# python_scripts/set_state.py
+# Create, update, or modify state and attributes in HA
+# Source: https://github.com/selfish/home-assistant-resources/blob/master/config/python_scripts/set_state.py
+# Adopted from https://github.com/rodpayne/home-assistant/blob/main/.homeassistant/python_scripts/set_state.py
 
 inputEntity = data.get("entity_id")
-if inputEntity is None:
-    logger.error("entity_id is required to set the state.")
-else:
-    try:
-        inputStateObject = hass.states.get(inputEntity)
-        inputState = inputStateObject.state if inputStateObject else None
-        inputAttributesObject = inputStateObject.attributes.copy() if inputStateObject else {}
+inputState = data.get("state", "unknown")
+allowCreate = data.get("allow_create", False)
 
-        for item, newAttribute in data.items():
-            if item in ["entity_id", "allow_create"]:
-                continue  # Skip already handled items
-            if item == "state":
-                inputState = newAttribute
-            else:
-                inputAttributesObject[item] = newAttribute
+if inputEntity is None:
+    logger.warning("===== entity_id is required if you want to set something.")
+else:
+    inputStateObject = hass.states.get(inputEntity)
+    if inputStateObject is None and not allowCreate:
+        logger.warning("===== unknown entity_id: %s", inputEntity)
+    else:
+        inputAttributesObject = {}
+        if inputStateObject is not None:
+            inputAttributesObject = inputStateObject.attributes.copy()
+
+        for item in data:
+            if item not in ["entity_id", "allow_create", "state"]:
+                inputAttributesObject[item] = data.get(item)
 
         hass.states.set(inputEntity, inputState, inputAttributesObject)
-        logger.info(f"Updated state of '{inputEntity}' to '{inputState}' with attributes {inputAttributesObject}.")
-
-    except Exception as e:
-        logger.error(f"Error updating state of '{inputEntity}': {e}")
